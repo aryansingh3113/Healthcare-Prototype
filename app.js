f581cbd4616f6c7d52a9b64a022eeadd2b8300d0
Aryan Singh <aryansinghb.246@gmail.com>
2024-07-24 17:43:07 +0530

    removed bugs

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
-document.addEventListener('DOMContentLoaded', (event) => {
-    var map = L.map('map').setView([51.505, -0.09], 13); // Set initial view
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Add a marker
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A sample marker')
-        .openPopup();
-});
